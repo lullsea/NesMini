12ca3c7018d6a54044c1acb9e5edc13cfc3d1785
lullsea <lullseapoint@gmail.com>
2022-12-19 13:42:44 +0800

    Started on graphical user interface

---
+++ NesMini/src/net/lullsea/NesMini/NesFrame.java
@@ -0,0 +1,20 @@
+package net.lullsea.NesMini;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class NesFrame extends JFrame {
+
+    NesFrame() {
+        super("Nes Mini - by Lull");
+        setSize(800, 900);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        getContentPane().setBackground(new Color(0x76583D));
+        setVisible(true);
+
+    }
+
+}
